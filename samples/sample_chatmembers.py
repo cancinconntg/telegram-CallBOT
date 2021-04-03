4d411ed51827a8c03d67ef7df521b43f41ecbf8e
cancinconntg <78117100+cancinconntg@users.noreply.github.com>
2021-04-03 04:37:59 +0300

    Update sample_chatmembers.py

--- samples/sample_chatmembers.py
+++ samples/sample_chatmembers.py
@@ -16,7 +16,7 @@ app = Client(creds.token, api_id=creds.id, api_hash=creds.hash)
 @app.on_message(Filters.command(["members"]))
 def calla(client, message):
     print("channel id:", message.chat.id)
-    participants = app.get_chat_members(chat_id=message.chat.id, offset=0, limit=200)
+    participants = app.get_chat_members(chat_id=message.chat.id, offset=0, limit=300)
     client.send_message(message.chat.id, "Participants: " + str(participants))
 
 
