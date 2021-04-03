2f25e3bd687fa49f3eb4b5cfad92948fefc56612
cancinconntg <78117100+cancinconntg@users.noreply.github.com>
2021-04-03 04:36:49 +0300

    Update main.py

--- main.py
+++ main.py
@@ -14,14 +14,14 @@ load_dotenv()
 
 # constants
 # ---------
-BOT_SESSION_NAME = os.getenv("BOT_SESSION_NAME")
-USER_SESSION_NAME = os.getenv("USER_SESSION_NAME")
-APP_ID = os.getenv("APP_ID")
-APP_HASH = os.getenv("APP_HASH")
-MONGO_URI = os.getenv("MONGO_URI")
-BOT_NAME = "bot_acc"
-USER_NAME = "user_acc"
-DB_NAME = "calls_db"
+BOT_SESSION_NAME = "AQCXkx4cYtxiBHlQe3Jy3bEwWDKR2Ic9Qhs9ZxdWDq661TsW8cVn9MNyDjAaNf8sTZ0NyIyWDyynPa_qQTYKfCKKp6T8I6pOVJ_4mp1y_Yklq91ZC5pZKNz8nWjot6ZIfmMSGAVJCBdfMCwPAoQ2jdcK6OhxtSCKe9HqzHfDSCBHrTTLGwsjSuIoO7LWjOyGAm0a-_IAOVJ8MYgdox1MvkYg5QGaRigNn2DzN_1xy6jLA6RyOgxbgzOub_f2Opzas8l77Ol9Ana-BBjMO2_vNcCg29bPDT63iSkWkit-FD2NREmm9j9Da598mpcIk84lVmvVlzfwb7mm0c1HpAY4ZoUlZzBLXgE"
+USER_SESSION_NAME = "AQAB9_FoFJ1jHpeE_gSzKp8AbrZOmT-BZzz_ZZD94uoslq83lH7M3KOSbQY236_1fjLdIkV2ouhEUd0pMNRN-zRHvRHaF4lC1M-Pe2C_vbt9_1XvZ8IEEgVu-z-mqgGTgAJly5nVVhsv_NkAlk0ov6Y3dY_attEIM_B-k-i7SQTtkuenOfbTiSEz6gvIra9UnGJ6hBT57WEIT06Cp-9bONH4cn-AP7NLPRdf0hH8qNMBCdxkbG25aYmW4MSvcRCZSXGGEIDDF9CVAkrJ1qEG36-h32M7jl8UTWlQtVRt2Za64Zn-RDWvAS3LzHpP0gQwIEXFA-0YPTznkwiGRz_T1xavYknMbwA"
+APP_ID = "3381924"
+APP_HASH = "b2bc3786b7fea440bb2e0a8ff39a09f9"
+MONGO_URI = "mongodb+srv://erich:erich@cluster0.b3rfv.mongodb.net/myFirstDatabase?retryWrites=true&w=majority"
+BOT_NAME = "Hrr"
+USER_NAME = "Can"
+DB_NAME = "Cluster0"
 
 
 def main():
